dd61f76297059d71231b7428f33b1eb0b1adca0c
bigacz <sbigacz@gmail.com>
2024-11-11 22:24:39 +0100

    restructure styling

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
+import 'src/styles/styleBarrel';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from 'src/App';
-import 'src/index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
---
+++ src/styles/styleBarrel.js
@@ -0,0 +1,3 @@
+import 'normalize.css';
+import 'src/styles/general.css';
+import 'src/styles/header.css';
