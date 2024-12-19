01495818106923e47d086098404579f7aa64bb37
bigacz <sbigacz@gmail.com>
2024-12-19 23:05:57 +0100

    style ExperiencePreview

--- src/components/preview/ExperiencePreview.jsx
+++ src/components/preview/ExperiencePreview.jsx
@@ -13,22 +13,30 @@ function ExperiencePreview({ jobs }) {
     } = job;
 
     const dutiesList = duties.map((duty) => {
-      return <p key={duty.id}>{duty.value}</p>;
+      return <li key={duty.id}>{duty.value}</li>;
     });
 
+    const sectionPrefix = 'experience-preview__jobs__section__';
+
     return (
-      <div key={id}>
-        <p>{companyName}</p>
-        <p>{positionTitle}</p>
-        {dutiesList}
-        <p>{yearStarted}</p>
-        <p>{yearEnded}</p>
-        <p>{isActive}</p>
+      <div key={id} className="experience-preview__jobs__section">
+        <div className={`${sectionPrefix}company`}>{companyName}</div>
+        <div className={`${sectionPrefix}position`}>{positionTitle}</div>
+        <ul className={`${sectionPrefix}duties`}>{dutiesList}</ul>
+        <div className={`${sectionPrefix}date`}>
+          {yearStarted} - {isActive ? 'Present' : yearEnded}
+        </div>
       </div>
     );
   });
 
-  return <div>{jobSections}</div>;
+  return (
+    <div className="experience-preview">
+      <div className="experience-preview__header">Experience</div>
+      <hr className="experience-preview__line" />
+      <div className="experience-preview__jobs">{jobSections}</div>
+    </div>
+  );
 }
 
 export default ExperiencePreview;
